d268c5b7eef4eb7635d6be5aa6a7f01fa29bcd44
noerr <1305115562@qq.com>
2021-11-20 13:18:34 +0800

    first commit

---
+++ merge_map.go
@@ -0,0 +1,18 @@
+package merge_map
+
+import "reflect"
+
+func mergeMap(src map[string]interface{}, new map[string]interface{}) {
+	for k := range new {
+		for k1 := range src {
+			if k == k1 {
+				if reflect.TypeOf(new[k]).Kind() == reflect.Map && reflect.TypeOf(src[k]).Kind() == reflect.Map {
+					mergeMap(src[k].(map[string]interface{}), new[k].(map[string]interface{}))
+				} else {
+					src[k1] = new[k]
+				}
+			}
+		}
+		src[k] = new[k]
+	}
+}
