3b5580e3988473acfe3158601b6db8c4d1e1da3b
noerr <1305115562@qq.com>
2021-11-23 21:31:37 +0800

    修复一个小bug

--- merge_map.go
+++ merge_map.go
@@ -7,7 +7,7 @@ func New2Src(src map[string]interface{}, new map[string]interface{}) {
 		for k1 := range src {
 			if k == k1 {
 				if reflect.TypeOf(new[k]).Kind() == reflect.Map && reflect.TypeOf(src[k]).Kind() == reflect.Map {
-					mergeMap(src[k].(map[string]interface{}), new[k].(map[string]interface{}))
+					New2Src(src[k].(map[string]interface{}), new[k].(map[string]interface{}))
 				} else {
 					src[k1] = new[k]
 				}
