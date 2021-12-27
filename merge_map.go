a13d8ea3fc15bd6cf8fbcd682ffee8c6800ab11e
lichuan <chuan.li@bangcle.com>
2021-12-27 13:57:41 +0800

    增加src2new map的

--- merge_map.go
+++ merge_map.go
@@ -16,3 +16,16 @@ func New2Src(src map[string]interface{}, new map[string]interface{}) {
 		src[k] = new[k]
 	}
 }
+func Src2New(src, new map[string]interface{}) {
+	for k := range src {
+		for k1 := range new {
+			if k == k1 {
+				if reflect.TypeOf(new[k]).Kind() == reflect.Map && reflect.TypeOf(src[k]).Kind() == reflect.Map {
+					Src2New(src[k].(map[string]interface{}), new[k].(map[string]interface{}))
+				} else {
+					src[k] = new[k]
+				}
+			}
+		}
+	}
+}
